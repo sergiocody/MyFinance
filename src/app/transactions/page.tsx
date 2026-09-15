3f3f1dfea107de28ad611cd2f65bd22b501c8037
Sergiocody <sergiocody@yahoo.es>
2026-09-15 14:08:09 +0200

    feat: enhance account filtering to include transfer accounts

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -289,7 +289,10 @@ export default function TransactionsPage() {
       .order("date", { ascending: false })
       .range(page * PAGE_SIZE, (page + 1) * PAGE_SIZE - 1);
 
-    if (filterAccount) query = query.eq("account_id", filterAccount);
+    if (filterAccount)
+      query = query.or(
+        `account_id.eq.${filterAccount},transfer_to_account_id.eq.${filterAccount}`
+      );
     if (filterCategory) query = query.eq("category_id", filterCategory);
     if (filterType) query = query.eq("type", filterType);
     if (filterDateFrom) query = query.gte("date", filterDateFrom);
